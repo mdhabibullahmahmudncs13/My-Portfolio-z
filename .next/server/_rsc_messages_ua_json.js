2f28fc0dbaf5014ae4e9627453b7cc7b19546ad2
MD Habibullah Mahmud <mdhabibullahmahmudncs13@gmail.com>
2025-06-24 09:04:39 +0600

    Added _rsc_messages_ua_json.js

---
+++ .next/server/_rsc_messages_ua_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_ua_json";
+exports.ids = ["_rsc_messages_ua_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/ua.json":
+/*!**************************!*\
+  !*** ./messages/ua.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"header":{"home":"головна","projects":"проекти","about":"про-себе","contacts":"контакти"},"footer":{"description":"Веб дизайнер та фронтенд розробник","copyright":"Усі права захищені 2023. Зробив Elias","media":"Медіа"},"skills":{"language":"Мови","database":"Бази даних","other":"Інше","tool":"Інструменти","framework":"Бібліотеки"},"projects":{"chertnodes":{"name":"ChertNodes","description":"Хостинг майнкрафт серверів"},"protectx":{"name":"ProtectX сайт","description":"Сайт для анти-краш дискорд бота"},"khanswers":{"name":"Kahoot Відповіді","description":"Отримати відповіді з кахут завдання"},"kotikbot":{"name":"КотикБот","description":"Сайт для мультифункціонального дискорд бота"},"portfolio":{"name":"Це портфоліо","description":"Моє персональне портфоліо"},"discordbot":{"name":"Шаблон дискорд бота","description":"Почни створювати дискорд бота з тайпскриптом за секунди"},"chesspro":{"name":"Chess PRO","description":"Сайт для перегляду шахових турнірів"},"madhost":{"name":"MadHost","description":"Хостинг сайтів и майнкрафт серверів з низькими цінами"},"feedrum":{"name":"Feedrum","description":"Українська соціальна мережа для програмістів"},"deplos":{"name":"Deplos","description":"Хостинг для серверів різних ігор"},"pixel-battle":{"name":"Pixel Battle","description":"Клон r/place"}},"pages":{"home":{"hero":{"title":"Elias - <span>веб дизайнер</span> та <span>фронт-енд</span> розробник","description":"Він створює адаптивні дизайни де перетинаються креативність і технології","button":"Мої контакти","status":"Відкрит для нових пропозицій"},"quote":{"text":"З великою потужністю приходить великий рахунок за електроенергію","author":"Доктор Хто"},"projects":{"title":"проекти","button":"Продивитися усе"},"skills":{"title":"скіли"},"about":{"title":"про-себе","description":["Я фронтенд-розробник самоучка із Києва. Я можу розробляти адаптивні веб-сайти з нуля і перетворювати їх на сучасні зручні веб-інтерфейси.","Перетворення моєї творчості та знань на веб-сайти є моєю пристрастю більше року. Я допомагаю різним клієнтам показати себе у мережі. Я завжди намагаюся дізнаватися про новітні технології та фреймворки."],"button":"Продивитися усе"},"contacts":{"title":"контакти","text":"Мене цікавлять можливості фрілансу. Однак, якщо у вас є інше питання, не соромтеся звертатися до мене.","media":"Пишіть мені сюди"}},"projects":{"description":"Мої проекти","decent":"непогані","small":"маленькі"},"about":{"description":"Хто я такий","about":{"description":["Я фронтенд-розробник самоучка із Києва. Я можу розробляти адаптивні веб-сайти з нуля і перетворювати їх на сучасні зручні веб-інтерфейси.","Перетворення моєї творчості та знань на веб-сайти є моєю пристрастю більше року. Я допомагаю різним клієнтам показати себе у мережі.","Я завжди намагаюся дізнаватися про новітні технології та фреймворки."]},"skills":{"title":"скіли"},"facts":{"title":"факти-про-мене","list":["Я дуже полюбляю зиму","Я часто їзджу на велосипеді з моїми друзями","Люблю піцу та пасту","Мій улюблений фільм це Зелена миля"]}},"contacts":{"description":"Як можно зі мною зв\'язатися"}}}');
+
+/***/ })
+
+};
+;
